055a89adf5b94340cebdc5b9de9f2927e6dc5d3b
KrisiStoyanova <56218445+KrisiStoyanova@users.noreply.github.com>
2022-11-28 15:58:49 +0200

    Add files via upload

---
+++ Homework4/ComputerPrices.java
@@ -0,0 +1,32 @@
+package Homework4;
+
+public class ComputerPrices {
+
+    public static void main(String[] args) {
+        Computer computer1 = new Computer(2021, 2599.99, "Microsoft");
+        Computer computer2 = new Computer();
+        computer2.getValues(2020, 3129.99, "macOS");
+        computer1.printPrice();
+        computer2.printPrice();
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+    //public static void Computer(String[] args) {
+
+
+
+    //public static int Computer(int computer2Year, double computer2Price, String computer2OperationSystem) {
+        //int computer2 = Computer(2020, 3129.99, "macOS");
+
+
