cdeea383adf375974244b94b8d2c87e4aa7d8ff8
KrisiStoyanova <56218445+KrisiStoyanova@users.noreply.github.com>
2023-01-10 10:53:07 +0200

    Add files via upload

---
+++ Homework6/AnimalDemo.java
@@ -0,0 +1,24 @@
+package Homework6;
+
+public class AnimalDemo {
+
+    public static void main(String[] args) {
+
+        Bird bird1 = new Bird();
+        bird1.play();
+        bird1.makeSomeNoise();
+
+        Cat cat1 = new Cat();
+        cat1.play();
+        cat1.makeSomeNoise();
+
+        Dog dog1 = new Dog();
+        dog1.play();
+        dog1.makeSomeNoise();
+
+    }
+    
+}
+
+
+
